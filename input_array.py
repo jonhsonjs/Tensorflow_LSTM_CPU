6954593de30dc5f8b42b7bd1febba1996b85cb5d
jonhsonjs <jhonshonjs@163.com>
2017-12-15 22:07:53 +0800

    first commit

--- input_array.py
+++ input_array.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.contrib.timeseries.python.timeseries import  NumpyReader
 
-
 x = np.array(range(1000))
 noise = np.random.uniform(-0.2, 0.2, 1000)
 y = np.sin(np.pi * x / 100) + x / 200. + noise
